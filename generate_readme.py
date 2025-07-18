ee2c2be038e7d35569cd3be1651b6c494ec1bbd1
刘博 <liubo1@gg.com>
2025-07-18 11:39:24 +0800

    [修改] TITIE

--- generate_readme.py
+++ generate_readme.py
@@ -59,7 +59,7 @@ def generate_index(policies):
 <html lang="zh">
 <head>
   <meta charset="UTF-8">
-  <title>RunGo 协议合集</title>
+  <title>协议合集</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style>
     body {
